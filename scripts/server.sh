a390d0719c243e02e87a5ef4ec9967d28c068772
inugami-dev64 <jxwest@protonmail.com>
2021-07-11 00:49:51 +0300

    Add git pull checker

---
+++ scripts/server.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# The purpose of this script is to index all the specified articles
+
+# Check if the template exists
+check_template() {
+    if [ -z $1 ]; then
+        echo "Please specify the index.html template"
+        exit 1
+    fi
+
+    if [ -z $2 ]; then
+        echo "Please specify the article template"
+        exit 1
+    fi
+}
+
+
+gen() {
+    LINKS=""
+    for f in `find ./articles -name *.md`; do
+        # sed is annoying
+        html=$(echo $f | sed "s/\.\(\/.*\)\.md/\1\.html/g")
+        html=$(echo $html | sed "s/markdown/html/g")
+        pandoc -s --template ./articles/templates/article.html -t html $f -o .$html
+
+        html=$(echo $html | sed "s/\//\\\ \//g")
+        html=$(echo $html | sed "s/ //g")
+
+        title=$(grep "title: " $(echo $f) | sed "s/^title: \(.*\)$/\1/g")
+        date=$(grep "date: " $(echo $f) | sed "s/^date: \(.*\)$/\1/g")
+        LINKS="$LINKS<li>$date - <a href=\"$html\">$title<\/a><\/li>\n"
+    done
+    echo -e $LINKS
+    echo -e $1
+
+    # Write the link list to the index file
+    sed -e "s/#LIST/$LINKS/g" $1 > index.html
+}
+
+
+while :; do
+    pull=$(git pull origin master 2>/dev/null | grep "Already up to date")
+
+    if [[ -z $pull ]]; then
+        check_template $1 $2
+        gen $1 $2
+    fi
+
+    sleep 1m
+done
