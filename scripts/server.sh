42fad934d785d46ec036d7533d0b6fe9c59aa44c
inugami-dev64 <jxwest@protonmail.com>
2021-07-11 13:25:02 +0000

    Fix sorted indexing in server script

--- scripts/server.sh
+++ scripts/server.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # The purpose of this script is to index all the specified articles
+IFS=""
 
 # Check if the template exists
 check_template() {
@@ -17,33 +18,27 @@ check_template() {
 
 gen() {
     LINKS=""
-    for f in `find ./articles -name *.md`; do
-        # sed is annoying
+    for f in ./articles/markdown/*.md; do
         html=$(echo $f | sed "s/\.\(\/.*\)\.md/\1\.html/g")
         html=$(echo $html | sed "s/markdown/html/g")
         pandoc -s --template ./articles/templates/article.html -t html $f -o .$html
 
-        html=$(echo $html | sed "s/\//\\\ \//g")
-        html=$(echo $html | sed "s/ //g")
-
         title=$(grep "title: " $(echo $f) | sed "s/^title: \(.*\)$/\1/g")
         date=$(grep "date: " $(echo $f) | sed "s/^date: \(.*\)$/\1/g")
 
-        LINKS="$LINKS<li>$date - <a href=\"$html\">$title<\/a><\/li>\n"
-        LINKS=$(echo -e $LINKS | sed -e "s/\./\\\ \./g")
-        LINKS=$(echo -e $LINKS | sed -e "s/ \./\./g")
+        LINKS="$LINKS<li>$date - <a href=$html>$title</a></li>\\\n"
     done
 
     # Write the link list to the index file
     LINKS=$(echo -e $LINKS | sort -r -d)
-    sed -e "s/#LIST/$LINKS/g" $1 > index.html
+    sed "s|#LIST|$LINKS|g" $1 > index.html
 }
 
 
 while :; do
     pull=$(git pull origin master 2>/dev/null | grep "Already up to date")
 
-    if [[ -z $pull ]]; then
+    if [[ -n $pull ]]; then
         check_template $1 $2
         gen $1 $2
     fi
@@ -51,6 +46,4 @@ while :; do
     sleep 1m
 done
 
-<li>2021-07-11 - <a href="\/articles\/html\/equipment.html">Equipment and software I use<\/a><\/li>
-<li>2021-07-10 - <a href="\/articles\/html\/hello.html">Hello Website<\/a><\/li>
-
+<li>2021-07-11 - <a href="\/articles\/html\/equipment\.html">Equipment and software I use<\/a><\/li>
